0b37cf98019cb8e1c281e86031cec423ad653a7f
yrm <yrm@na-s.jp>
2021-04-28 14:32:33 +0900

    [add] judge

--- back.js
+++ back.js
@@ -7,13 +7,16 @@ import { decode as base64decode } from "https://deno.land/std@0.95.0/encoding/ba
 
 
 async function doAuth(ctx, next){
+    let id;
     let user;{
         const auth = ctx.request.headers.get("Authorization");
         if(auth){
             const userpass = (new TextDecoder().decode(base64decode( auth.split(" ")[1] ))).split(":");
 
             const db = new DB("joicon.db");{
-                user = [...db.query("select name from TJudge where name=? and pass=?", [userpass[0], userpass[1]]).asObjects()][0]?.name;
+                const r = [...db.query("select id,name from TJudge where name=? and pass=?", [userpass[0], userpass[1]]).asObjects()][0];
+                id = r?.id;
+                user = r?.name;
                 db.close();
             }
         }
@@ -21,6 +24,7 @@ async function doAuth(ctx, next){
 
     if(user){
         console.log(`'${user}' accessed.`);
+        ctx.judgeid = id;
         await next();
     }else{
         ctx.response.status = 401;
@@ -32,7 +36,7 @@ const router = new Router();{
     router.get("/entries", async function(ctx){
         let r = null;
         const db = new DB("joicon.db");{
-            r = [...db.query("select id,sTitle,bThumb,datetime(dCreated,'+9 hours') as dCreatedJST from TEntry order by id").asObjects()];
+            r = [...db.query("select id,sTitle,bThumb,datetime(TEntry.dCreated,'+9 hours') as dCreatedJST,nJudgment from TEntry left outer join TJudgment on TEntry.id=TJudgment.pEntry and TJudgment.pJudge=? order by id", [ctx.judgeid]).asObjects()];
             db.close();
         }
         ctx.response.body = r;
@@ -56,6 +60,25 @@ const router = new Router();{
         }
         ctx.response.type = "application/octet-stream";
         ctx.response.body = r;
+    });
+
+    router.post("/judgment", async function(ctx){
+        const v = await ctx.request.body().value;
+        console.log( v );
+
+        const db = new DB("joicon.db");
+        db.query("BEGIN");{
+            for(const j of v){
+                db.query("DELETE FROM TJudgment WHERE pJudge=? and pEntry=?", [ctx.judgeid, j.entryid]);
+                if(j.judgment){
+                    db.query("INSERT INTO TJudgment (pJudge,pEntry,nJudgment) VALUES (?,?,?)", [ctx.judgeid, j.entryid, j.judgment]);
+                }
+                // db.query("UPDATE TJudgment set nJudgment=?,dJudged=CURRENT_TIMESTAMP WHERE pJudge=? and pEntry=?", [j.judgment??0, ctx.judgeid, j.entryid]);
+            }
+            db.query("COMMIT");
+            db.close();
+        }
+        ctx.response.body = { message: "OK" };
     });
 }
 
