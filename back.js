310b40cb66718df7f992bf66c5058c722e3b03d3
yrm <yrm@na-s.jp>
2021-05-02 12:58:12 +0900

    [add]back: judgment sort

--- back.js
+++ back.js
@@ -27,7 +27,7 @@ const router = new Router();{
     router.get("/entries", async function(ctx){
         let r = null;
         const db = new DB("joicon.db");{
-            r = [...db.query("select id,sTitle,bThumb,datetime(TEntry.dCreated,'+9 hours') as dCreatedJST from TEntry order by id").asObjects()];
+            r = [...db.query("select id,sTitle,bThumb,datetime(TEntry.dCreated,'+9 hours') as dCreatedJST,sum(nJudgment) as nJudgment from TEntry left outer join TJudgment on id=pEntry group by id order by id").asObjects()];
             db.close();
         }
         ctx.response.body = r;
