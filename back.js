e3a98a4e7010ff1f707406febecb97e486aafa3c
yrm <yrm@na-s.jp>
2021-05-04 20:20:55 +0900

    [add]back: judgment total / judge

--- back.js
+++ back.js
@@ -95,7 +95,7 @@ const router = new Router();{
     router.get("/judges", async function(ctx){
         let r = null;
         const db = new DB("joicon.db");{
-            r = [...db.query("select id,sName,sPass,(select count(*) from TJudgment where pJudge=id) as nJudged from TJudge order by id").asObjects()];
+            r = [...db.query("select id,sName,sPass,(select count(*) from TJudgment where pJudge=id) as nJudged,(select sum(nJudgment) from TJudgment where pJudge=id) as nJudgmentTotal from TJudge order by id").asObjects()];
             db.close();
         }
         ctx.response.body = r;
